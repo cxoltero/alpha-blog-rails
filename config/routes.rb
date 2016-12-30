1a81a194e485c6f3ecea4adad195f9cb89472a78
Carlos Soltero <carlosh95@gmail.com>
2016-12-30 15:21:11 -0500

    add signup for users

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
 
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
+
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
