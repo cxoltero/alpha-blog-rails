3b1091930e613eb023b43139b72ce51b992f1099
Carlos Soltero <carlosh95@gmail.com>
2016-12-30 12:55:30 -0500

    new user migration and model

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ActiveRecord::Base
+  validates :username, presence: true, length: {minimum: 3, maximum: 50};
+  validates :email, presence: true, length: {minimum: 10, maximum: 50};
+  has_many :articles
+end
