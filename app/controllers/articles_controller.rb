c82c29f45bd45a7876ee599893cec6e3d7e62997
Carlos Soltero <carlosh95@gmail.com>
2016-12-30 18:34:08 -0500

    update articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
     @article.user = User.first
     if @article.save
       flash[:success] = "Article was successfully created"
-      redirect_to article_path(@article);
+      redirect_to article_path(@article)
     else
       render 'new'
     end
