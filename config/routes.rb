53cf34e4b830035477525b0939364e4ce8f212af
Carlos Soltero <carlosh95@gmail.com>
2016-12-30 16:13:27 -0500

    add partial for user new and edit

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
 
   get 'signup', to: 'users#new'
-  post 'users', to: 'users#create'
 
   resources :articles
+  resources :users, except: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
