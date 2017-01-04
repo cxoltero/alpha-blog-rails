59f256ec88d43a5241a85722f125583f90fcdf9a
Carlos Soltero <carlosh95@gmail.com>
2017-01-03 21:59:55 -0500

    updated navbar to check if user is logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def index
     @users = User.paginate(page: params[:page], per_page: 9)
   end
@@ -18,11 +24,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your user information has been updated successfully."
       redirect_to articles_path
@@ -32,7 +36,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 6)
   end
 
