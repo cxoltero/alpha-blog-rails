8dff24a9d547aefae9725f5d772f658499f99b12
Carlos Soltero <carlosh95@gmail.com>
2017-01-11 21:54:48 -0500

    set restriction for editing and delting other user profiles and articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def set_user
     @user = User.find(params[:id])
@@ -42,5 +44,12 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:username, :email, :password);
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "You can only edit or delete your own account."
+      redirect_to root_path
+    end
   end
 end
